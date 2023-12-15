594fc7456ca449cc72cb61a401438e1dd958d227
Stone Olguin <olguinstone@gmail.com>
2023-12-15 15:45:20 -0700

    Finished work on project. Cleaned up code

--- project.py
+++ project.py
@@ -3,26 +3,40 @@ from z3 import *
 from tqdm import trange
 from bs4 import BeautifulSoup
 import re
+from tabulate import tabulate
 
-# Converts a boolean string to the boolean type
 def true_false_string_to_bool(tf_str):
+    """
+    Returns a boolean from a string that is true or false.
+
+    Parameters:
+    - tf_str (str): A string that is true or false
+
+    Returns:
+    - bool: The boolean value of the string
+
+    """
     ret_val = False
-    match tf_str:
+    match tf_str.lower():
         case "true":
             ret_val = True 
         case "false":
             ret_val = False
         case _:
             print(f"There was an error: input string {tf_str} is not a string of true or false")
-    # Return the converted boolean
     return ret_val
 
-# Reads through a given xml file and returns a pandas dataframe with the relevant data
 def get_data_from_xml(filename):
-    ## processes_data will store all of the relevant data from tags:
-    ##      Interval
-    ##      Sent_Info
-    ##      Receive_Info
+    """
+    Reads a given xml file for the trace and returns the relevant information.
+
+    Parameters:
+    - filename (str): A string that the name of the .xml file
+
+    Returns:
+    - dict: a dictionary containing the relevant information of the distributed trace
+
+    """
     processes_data = {}
     for i in range(10):
         processes_data[str(i)] = {}
@@ -85,9 +99,22 @@ def get_data_from_xml(filename):
     # After adding all the data into the dictionary, return it
     return processes_data
 
-# Determines whether the split to communication at receive time was a send or a receive
-# Returns a boolean
 def received_message(d, process, receive_time):
+    """
+    Returns a boolean on whether a given process received a message (or sent it)
+
+    Parameters:
+    - d (dict): Dictionary containing information about the distributed trace
+    - process (str): The string name of the process
+    - receive_time (str): An integer string that is the time a process cut to communication
+
+    Returns:
+    - bool: A boolean that determines whether that process received a message
+
+    Raises:
+    - KeyError: An error that says communication occurred, but it does not exist
+
+    """
     # Check if a message was received at the given time
     if receive_time in d[process]["receive_info"]:
         return True 
@@ -95,97 +122,29 @@ def received_message(d, process, receive_time):
     elif receive_time in d[process]["sent_info"]:
         return False
     else:
-        exit(f"Misc to communication when no communication occurred! Process, time: ({process}, {receive_time})") 
+        raise KeyError(f"Misc to communication when no communication occurred! Process, time: ({process}, {receive_time})") 
     
 
-# Runs z3 on the xml trace, and it returns whether it is satisified or not
-def run_z3(d):
+def run_z3(d, prev_run_info = [], run_again = False):
+    """
+    Runs Z3 on a distributed program trace and satisfies the message passing predicate.
 
+    Parameters:
+    - d (dict): Dictionary containing information about the distributed trace
+
+    Optional Parameters:
+    - prev_run_info (List 3-Tuple): Information about the previous run (used only in rerunning). Default is []
+    - run_again (bool): Boolean that states whether this function is being run again (used only in rerun). Default is False
+
+    Returns:
+    - None
+
+    """
     # Names of each entries' interval for debugging if failed:
     invalid_info = []
-    s = Solver();
-    s.set("timeout", 1000000)
-    # Add each past and current value to its index
-    for process in d.keys():
-        proc_i_intervals = d[process]["interval"]
-        for end_time in proc_i_intervals.keys():
-            (start_time, current_val, old_val, _) = proc_i_intervals[end_time]
-            previous = old_val
+    if run_again:
+        print("Rerunning the solver!\n")
 
-            # Used to determine if we need to check both previous and current values match
-            previous_and_current = False
-            
-            # Ensure that each process starts with x = True
-            if start_time == "0":
-                curr_of_previous = True
-            else:
-                # p_int is the previous interval (the start time of this is the end time of the last)
-                p_int = d[process]["interval"][start_time]
-
-                # If the previous interval involved communication:
-                if p_int[3]:
-                    received = received_message(d, process, start_time)
-
-                    # If the previous sent a message, grab its old value
-                    if not received:
-                        # Sending a message changes old to new
-                        curr_of_previous = p_int[2]
-
-                    # If the previous received a message, want to make sure the current_value is the same as the one received
-                    elif received: 
-                        # Previous entry's received value is the same current value
-                        current = current_val
-                        curr_of_previous = p_int[1]
-                        prev_of_previous = p_int[2]
-                        previous_and_current = True
-
-                    # Error
-                    else:
-                        exit("Error on saying communication, but could't find if it received the message!")
-                # Otherwise, just check the previous interval's value
-                else:
-                    curr_of_previous = p_int[1]
-                    
-            # If we have to add previous and current checks
-            if previous_and_current:
-                bv = BoolVal(current == curr_of_previous)
-                s.add(bv)
-                if (is_false(bv)):
-                    # First True is to say tested previous and current, second true to to say current and current compared
-                    invalid_info.append((process, start_time, end_time, current, curr_of_previous, True, True))
-                bv = BoolVal(previous == prev_of_previous)
-                s.add(bv)
-                if (is_false(bv)):
-                    invalid_info.append((process, start_time, end_time, previous, prev_of_previous, True, False))
-            else:
-                bv = BoolVal(previous == curr_of_previous)
-                s.add(bv)
-                if (is_false(bv)):
-                    invalid_info.append((process, start_time, end_time, previous, curr_of_previous, False, False))
-
-
-    if(s.check() == sat):
-        print(f"Constraints Satisified! The predicate maintained its value correctly!"), 
-    else:
-        print("Constraints Not Satisified! Here is where it went wrong: ")
-        for val in invalid_info:
-
-            if val[5]:
-                if val[6]:
-                    ## Had current and current compares
-                    print(f"\tProcess {val[0]} (start, end) ({val[1]}, {val[2]}): Current Value is {val[3]} when it should be {val[4]}")
-                else:
-                    print(f"\tProcess {val[0]} (start, end) ({val[1]}, {val[2]}): Old Value is {val[3]} when it should be {val[4]}")
-            else:
-                print(f"\tProcess {val[0]} (start, end) ({val[1]}, {val[2]}): Old Value is {val[3]} when it should be {val[4]}")
-        sys.stdout.flush()
-
-        rerun_z3(d, invalid_info)
-
-    return
-
-def rerun_z3(d, prev_run_info):
-    print("\nRerunning the solver!\n\n")
     # Set up z3 solver
     s = Solver();
     s.set("timeout", 1000000)
@@ -194,11 +153,13 @@ def rerun_z3(d, prev_run_info):
     for process in d.keys():
         proc_i_intervals = d[process]["interval"]
         for end_time in proc_i_intervals.keys():
+
+            # Information for rerunning to solve the errors
             invalid_curr = False
             invalid_prev = False
             (start_time, current_val, old_val, _) = proc_i_intervals[end_time]
             if len(prev_run_info) > 0:
-                (p, s_t, e_t, _, _ , _, _) = prev_run_info[0]
+                (p, s_t, e_t) = prev_run_info[0]
                 if p == process and s_t == start_time and e_t == end_time:
                     invalid_curr = True
                     current = Bool(f"Current_{p}_{s_t}_{e_t}")
@@ -254,11 +215,16 @@ def rerun_z3(d, prev_run_info):
                 else:
                     curr_of_previous = p_int[1]
                     
+            # Case statements to determine how to pass the argument to Z3:
             if previous_and_current and not invalid_prev and not invalid_curr:
                 bv = BoolVal(current == curr_of_previous)
                 s.add(bv)
+                if (is_false(bv)):
+                    invalid_info.append((process, start_time, end_time))
                 bv = BoolVal(previous == prev_of_previous)
                 s.add(bv)
+                if (is_false(bv)):
+                    invalid_info.append((process, start_time, end_time))
             elif previous_and_current and not invalid_prev and invalid_curr:
                 s.add(current == BoolVal(curr_of_previous))
                 s.add(previous == BoolVal(prev_of_previous))
@@ -272,36 +238,92 @@ def rerun_z3(d, prev_run_info):
             else:
                 bv = BoolVal(previous == curr_of_previous)
                 s.add(bv)
+                if (is_false(bv)):
+                    invalid_info.append((process, start_time, end_time))
 
 
+    # If we satisfied
     if(s.check() == sat):
         print(f"Constraints Satisified! The predicate maintained its value correctly!")
-        print("Here are the fixes suggested for the errors: \n")
-        m = s.model()
-        pattern = r'\d+'
-        for d in m.decls():
-            nums = re.findall(pattern, d.name())
-            if re.search(r'Old', d.name()):
-                val = "Old"
-            else: 
-                val = "New"
-            if len(nums) != 3:
-                print("ERROR: Not three numbers in name!")
-            else:                     ## We have the name of the old value too
-                process = nums[0]
-                start_time = nums[1]
-                end_time = nums[2]
-                solution = m[d]
-                print(f"\tProcess {process}: (start, end): ({start_time}, {end_time}):")
-                print(f"\t\tChange {val} value to {solution}")
-    else:
-        print("Constraints Not Satisified!\n ERROR: It should not go wrong again!")
+
+        # If we ran again, show how to fix the errors
+        if run_again:
+            print("Here are the changes and checks suggested for the errors: \n")
+            m = s.model()
+            pattern_vals = r'\d+'
+            pattern_old = r'Old'
+            # Solutions dictionary to print after
+            solutions_print = {}
+
+            for i, d in enumerate(m.decls()):
+                # Grab information about the solution and add it to table
+                solutions_print[str(i)] = {}
+                nums = re.findall(pattern_vals, d.name())
+                if len(nums) != 3:
+                    print("ERROR: Not three numbers in name!")
+                else:          
+                    process = nums[0]
+                    start_time = nums[1]
+                    end_time = nums[2]
+                    solution = m[d]
+                    if re.search(pattern_old, d.name()):
+                        val = "Old"
+                    else: 
+                        val = "New"
+                    solutions_print[str(i)]["Process"] = process
+                    solutions_print[str(i)]["Start_Time"] = start_time
+                    solutions_print[str(i)]["End_Time"] = end_time
+                    solutions_print[str(i)]["Change:"] = f"{val} value to {str(solution).lower()}"
+
+            # Print solutions
+            df = pd.DataFrame.from_dict(solutions_print).T
+            df = tabulate(df, headers="keys", tablefmt = 'grid', showindex=True, \
+                          colalign=("center", "center", "center", "center", "center"))
+            print(df)
+            return
+        
+    # If we ran again and failed: is an error
+    elif run_again:
+        print("Constraints Not Satisified!\nERROR: It should not go on the second run!")
         sys.stdout.flush()
+        return
+    # If we ran the first time and didn't satisfy: Display what is wrong and ask if it wants to be solved
+    else:
+        print("Constraints Not Satisified! Here is where it went wrong: ")
+
+        # Print information about failed intervals:
+        invalid_print = {}
+        for i, val in enumerate(invalid_info):
+            invalid_print[str(i)] = {}
+            invalid_print[str(i)]["Process"] = val[0]
+            invalid_print[str(i)]["Start_Time"] = val[1]
+            invalid_print[str(i)]["End_Time"] = val[2] 
+        sys.stdout.flush()
+        df = pd.DataFrame.from_dict(invalid_print).T 
+        df = tabulate(df, headers="keys", tablefmt = 'grid', showindex=True, \
+                      colalign=("center", "center", "center", "center"))
+        print(df)
+
+        # Prompt the user to run again
+        print("\nWant to run again? Y for yes, N for no")
+        while not run_again:
+            response = input("\t").strip().lower()
+            if response in ('y', 'yes'):
+                run_again=True
+                run_z3(d, invalid_info, True)
+            elif response in ('n', 'no'):
+                return
+            else:
+                print("Ivalid input. Please enter Y for yes or N for no. ")
+        return
+
 
 ## Main function
 def main():
+    # Get the data from the trace
     d = get_data_from_xml("trace.xml")
 
+    # Run z3 on the data
     run_z3(d)
     return
 
